21cdd82554ef83ba707aaf83c606060154ac4898
dizlu <golmarcin@gmail.com>
2017-11-16 21:50:55 +0100

    working proper calculations

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -79,6 +79,10 @@
       </form>
     </section>
 
+    <section id="connections">
+      <button>Add new connection</button>
+    </section>
+
 
   </div>
 </template>
@@ -112,19 +116,23 @@ export default {
         triangle: {
           name: 'Triangle',
           ranges: [0, 0, 0],
-          value: (ranges, value) => {
+          value: (rangesParam, valueParam) => {
+            const ranges = rangesParam.map(range => parseInt(range, 10));
+            const value = parseInt(valueParam, 10);
             if (value < ranges[0] || value > ranges[2]) return 0;
-            if (value <= ranges[1]) return (value - ranges[0]) / (ranges[1] - ranges[0]);
+            if (value < ranges[1]) return (value - ranges[0]) / (ranges[1] - ranges[0]);
             return (ranges[2] - value) / (ranges[2] - ranges[1]);
           },
         },
         trapezoid: {
           name: 'Trapezoid',
           ranges: [0, 0, 0, 0],
-          value: (ranges, value) => {
+          value: (rangesParam, valueParam) => {
+            const ranges = rangesParam.map(range => parseInt(range, 10));
+            const value = parseInt(valueParam, 10);
             if (value < ranges[0] || value > ranges[3]) return 0;
-            if (value <= ranges[1]) return (value - ranges[0]) / (ranges[1] - ranges[0]);
-            else if (value <= ranges[2]) return 1;
+            if (value < ranges[1]) return (value - ranges[0]) / (ranges[1] - ranges[0]);
+            else if (value < ranges[2]) return 1;
             return (ranges[3] - value) / (ranges[3] - ranges[2]);
           },
         },
